ac42b3b6f4e42dc219f2aa30af97302956daaafe
pixotchi <153137364+pixotchi@users.noreply.github.com>
2025-09-12 19:07:14 +0200

    Add files via upload

---
+++ public/abi/staking-abi.ts
@@ -0,0 +1,35 @@
+export const stakingAbi = [
+  {
+    "inputs": [ { "internalType": "uint256", "name": "amount", "type": "uint256" } ],
+    "name": "stake",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [ { "internalType": "uint256", "name": "amount", "type": "uint256" } ],
+    "name": "withdraw",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "claimRewards",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [ { "internalType": "address", "name": "user", "type": "address" } ],
+    "name": "getStakeInfo",
+    "outputs": [
+      { "internalType": "uint256", "name": "staked", "type": "uint256" },
+      { "internalType": "uint256", "name": "rewards", "type": "uint256" }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  }
+];
+
+
