b28515eb397b02b3be703ec9ef0bb3711fe1862e
pixotchi <team@pixotchi.tech>
2025-10-18 04:47:17 +0200

    .

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactCompiler: true,
-  // Silence warnings
-  // https://github.com/WalletConnect/walletconnect-monorepo/issues/1908
-  webpack: (config) => {
-    config.externals.push("pino-pretty", "lokijs", "encoding");
-    return config;
-  },
-  // External packages for server components
-  serverExternalPackages: []
+  serverExternalPackages: ["pino-pretty", "lokijs", "encoding"],
+  turbopack: {}
 };
 
 export default nextConfig;
