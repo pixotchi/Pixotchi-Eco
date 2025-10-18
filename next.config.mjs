13df65b7ed6ac2429cedc9837034882323642f20
pixotchi <team@pixotchi.tech>
2025-10-18 04:41:33 +0200

    next16

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactCompiler: true,
   // Silence warnings
   // https://github.com/WalletConnect/walletconnect-monorepo/issues/1908
   webpack: (config) => {
