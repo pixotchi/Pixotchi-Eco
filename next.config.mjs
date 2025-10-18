ad9834d55ae80f89443ff00fafd5e8095686afcb
pixotchi <team@pixotchi.tech>
2025-10-19 00:29:10 +0200

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  turbopack: {},
   // Silence warnings
   // https://github.com/WalletConnect/walletconnect-monorepo/issues/1908
   webpack: (config) => {
